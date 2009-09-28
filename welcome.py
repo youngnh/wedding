e22b8e09a01093524571d26ca98f00494752e0f3
soafa-user <n@spectre.(none)>
2009-09-27 23:07:09 -0500

    now all pages go through template template hooked up

--- welcome.py
+++ welcome.py
@@ -6,11 +6,64 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 
 class MainPage(webapp.RequestHandler):
     def get(self):
-        path = os.path.join(os.path.dirname(__file__), 'welcome.html')
+        path = os.path.join(os.path.dirname(__file__), self.getPath())
         self.response.out.write(template.render(path, {}))
 
-application = webapp.WSGIApplication([('/', MainPage)],
+class AboutUsPage(MainPage):
+    def getPath(self):
+        return 'aboutus.html'
+
+class CeremonyPage(MainPage):
+    def getPath(self):
+        return 'ceremony.html'
+
+class GuestInformationPage(MainPage):
+    def getPath(self):
+        return 'guestinformation.html'
+
+class HoneymoonPage(MainPage):
+    def getPath(self):
+        return 'honeymoon.html'
+
+class OurProposalPage(MainPage):
+    def getPath(self):
+        return 'ourproposal.html'
+
+class PhotoAlbumPage(MainPage):
+    def getPath(self):
+        return 'photoalbum.html'
+
+class ReceptionPage(MainPage):
+    def getPath(self):
+        return 'reception.html'
+
+class RegistryPage(MainPage):
+    def getPath(self):
+        return 'registry.html'
+
+class WeddingPartyPage(MainPage):
+    def getPath(self):
+        return 'weddingparty.html'
+
+class WelcomePage(MainPage):
+    def getPath(self):
+        return 'welcome.html'
+
+application = webapp.WSGIApplication([('/aboutus.html', AboutUsPage),
+                                      ('/ceremony.html', CeremonyPage),
+                                      ('/guestinformation.html', GuestInformationPage),
+                                      ('/honeymoon.html', HoneymoonPage),
+                                      ('/ourproposal.html', OurProposalPage),
+                                      ('/photoalbum.html', PhotoAlbumPage),
+                                      ('/reception.html', ReceptionPage),
+                                      ('/reception.html', ReceptionPage),
+                                      ('/registry.html', RegistryPage),
+                                      ('/weddingparty.html', WeddingPartyPage),
+                                      ('/welcome.html', WelcomePage),
+                                      ('/', WelcomePage)],
                                      debug=True)
+
+
 def main():
     run_wsgi_app(application)
 
