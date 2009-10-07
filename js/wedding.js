ae6f877aba1f4946a5cf52eeb72301c02bb99740
Nate Young <n@spectre.(none)>
2009-10-06 20:25:16 -0500

    converted countdown code to jquery

--- js/wedding.js
+++ js/wedding.js
@@ -10,11 +10,11 @@ function menuRollout(event) {
 
 function countdown() {
     var today = new Date().getTime();
-    var bigDay = new Date($('big_day').textContent).getTime();
+    var bigDay = new Date($('#big_day').text()).getTime();
 
     var daysToGo = Math.ceil(((((bigDay - today) / 1000) / 60) / 60) / 24);
 
-    $('days_to_go').update(daysToGo + " days to go!");
+    $('#days_to_go').replaceWith(daysToGo + " days to go!");
 }
 
 function wireUpMenu() {
